a005ad7ef6c3fa8fb8d10d0db7ee6ea6608180f5
Happy Patel <ah9265609130@gmail.com>
2026-04-11 17:25:16 +0530

    Added run_icarus.bat and made necessary changes to run simulation

---
+++ sim/run/run_icarus.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d %~dp0
+
+iverilog -o sim.out -I ..\..\rtl\include ..\..\rtl\core\top.v ..\..\rtl\core\compute_unit.v ..\..\rtl\warp\warp_manager.v ..\..\rtl\pipeline\IFU.v ..\..\rtl\pipeline\decode_unit.v ..\..\rtl\pipeline\execute_stage.v ..\..\rtl\pipeline\mem_stage.v ..\..\rtl\pipeline\writeback_stage.v ..\..\rtl\alu\vector_alu.v ..\..\rtl\register_file\vector_register_file.v ..\..\rtl\memory\instruction_memory.v ..\..\rtl\memory\data_memory.v ..\..\tb\unit\tb_compute_unit.v
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Compilation failed
+    pause
+    exit /b
+)
+
+cd ..
+vvp run\sim.out
+pause
